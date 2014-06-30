e93ef5cf65109b0aebe556f5be22b7fcee3e7523
won <wjin@nyit.edu>
2014-06-30 04:29:34 -0400

    edit api/beta files

---
+++ cloud/models/beta/index.php
@@ -0,0 +1,12 @@
+<?php
+
+	require_once($_SERVER['DOCUMENT_ROOT']. 'cloud/modes/main/index.php');
+
+	Function betaSignup($email){
+
+		dbQuery("INSERT INTO beta (email) VALUES ('$email')");
+		$resp = array("status"=>"success", "reason"=> "email added to beta")
+
+		return($resp);
+	}
+?>
