e7c37268e1c45b26340237c956da6e6cb50ee4f4
won <wjin@nyit.edu>
2014-06-30 20:19:00 -0400

    fixing merge conflict

--- cloud/models/beta/index.php
+++ cloud/models/beta/index.php
@@ -1,29 +1,17 @@
 <?php
 
-<<<<<<< HEAD
-	require_once($_SERVER['DOCUMENT_ROOT'].'cloud/models/main/index.php');
-
-	Function betaSignup($email){
-
-		dbQuery("INSERT INTO beta (email) VALUES ('$email')");
-		$resp = array("status"=>"success", "reason"=> "email added to beta")
-
-		return($resp);
-	}
-=======
 	//include main functionity (DB connect, etc)
 	require_once($_SERVER['DOCUMENT_ROOT'].'cloud/models/main/index.php');
-	
+
 	function betaSignup($email){
 
 		dbQuery("INSERT INTO beta (email) VALUES ('$email')");
 		$resp  = array("status"=>"success", "reason"=>"email added to beta");
 		return($resp);
 	}
-	
 
 
 
 
->>>>>>> 5473eb99b4dac31ab3beeac411694f6bbab15688
+
 ?>
