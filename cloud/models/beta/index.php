3d0d68d16ce1921f1948c4119eccd9167a624bc0
won <wjin@nyit.edu>
2014-07-22 20:45:40 -0400

    dssd

--- cloud/models/beta/index.php
+++ cloud/models/beta/index.php
@@ -3,7 +3,7 @@
 	//include main functionity (DB connect, etc)
 	require_once($_SERVER['DOCUMENT_ROOT'].'cloud/models/main/index.php');
 
-	function betaSignup(email){
+	function betaSignup($email){
 
 		dbQuery("INSERT INTO beta (email) VALUES ('$email')");
 		$resp = array("status"=>"success", "reason"=>"beta user added");
